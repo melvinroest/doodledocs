b1684ecb90e2184387ec056912e1f4a2ce51d154
melvin roest <melvinroest@gmail.com>
2019-10-01 16:54:06 +0200

    added scroll-lock library and added ember auto import

--- app/components/doodledoc.js
+++ app/components/doodledoc.js
@@ -1,8 +1,10 @@
 import Component from '@ember/component';
+import { disablePageScroll } from 'scroll-lock';
 
 // When person is drawing, put everything else on non-clickable (and visually display it by greying it out)
 
 function init(canvas){
+  disablePageScroll();
   let context = canvas.getContext('2d');
   canvas.style.width ='100%';
   canvas.style.height='100%';
@@ -18,7 +20,6 @@ export default Component.extend({
   style: "position: relative; height: 100vh;",
   didInsertElement() {
     this._super(...arguments);
-    console.log([this.element.firstChild])
     if(this.element.childNodes.length === 1 && this.element.childNodes[0].nodeName === "CANVAS"){
       init(this.element.childNodes[0]);  
     }
